lowRISC/opentitan

[rust] move to new version of serde-annotate
This removes the need of using min_specialization feature and allows us
to move to the stable compiler.

Signed-off-by: Gary Guo <gary.guo@lowrisc.org>

sw/host/opentitanlib/opentitantool_derive/src/lib.rs
@@ -31,7 +31,7 @@ use syn::{parse_macro_input, Data, DataEnum, DeriveInput, Error, Fields, Ident, 
 ///         &self,
 ///         context: &dyn std::any::Any,
 ///         backend: &mut dyn opentitanlib::transport::Transport
-///     ) -> anyhow::Result<Option<Box<dyn serde_annotate::Annotate>>> {
+///     ) -> anyhow::Result<Option<Box<dyn serde_annotate::AnnotateSerialize>>> {
 ///         match self {
 ///             Hello::World(ref __field) => __field.run(context, backend),
 ///             Hello::People(ref __field) => __field.run(context, backend),
@@ -84,7 +84,7 @@ fn dispatch_enum(name: &Ident, e: &DataEnum) -> Result<TokenStream> {
                     &self,
                     context: &dyn std::any::Any,
                     backend: &opentitanlib::app::TransportWrapper,
-                ) -> anyhow::Result<Option<Box<dyn serde_annotate::Annotate>>> {
+                ) -> anyhow::Result<Option<Box<dyn serde_annotate::AnnotateSerialize>>> {
                     match self {
                         #(#arms),*
                     }

swechchhaad/kaiburr-acc

sw/host/opentitanlib/opentitantool_derive/src/lib.rs
@@ -31,7 +31,7 @@ use syn::{Data, DataEnum, DeriveInput, Error, Fields, Ident, Result, Variant, pa
 ///         &self,
 ///         context: &dyn std::any::Any,
 ///         backend: &mut dyn opentitanlib::transport::Transport
-///     ) -> anyhow::Result<Option<Box<dyn serde_annotate::Annotate>>> {
+///     ) -> anyhow::Result<Option<Box<dyn erased_serde::Serialize>>> {
 ///         match self {
 ///             Hello::World(ref __field) => __field.run(context, backend),
 ///             Hello::People(ref __field) => __field.run(context, backend),
@@ -84,7 +84,7 @@ fn dispatch_enum(name: &Ident, e: &DataEnum) -> Result<TokenStream> {
                     &self,
                     context: &dyn std::any::Any,
                     backend: &opentitanlib::app::TransportWrapper,
-                ) -> anyhow::Result<Option<Box<dyn serde_annotate::Annotate>>> {
+                ) -> anyhow::Result<Option<Box<dyn erased_serde::Serialize>>> {
                     match self {
                         #(#arms),*
                     }
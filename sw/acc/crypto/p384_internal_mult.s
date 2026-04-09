0cbcc8ec7f94c2221ba59b4625331d7a2dc422a7
Kat Fox <kat@zerorisc.com>
2026-04-09 15:03:53 +0100

    [crypto] Standardize file header format for cryptolib and related files
    Signed-off-by: Kat Fox <kat@zerorisc.com>

--- sw/acc/crypto/p384_internal_mult.s
+++ sw/acc/crypto/p384_internal_mult.s
@@ -4,6 +4,7 @@
 
 
 /* Copyright lowRISC contributors (OpenTitan project). */
+/* Copyright zeroRISC Inc. */
 /* Licensed under the Apache License, Version 2.0, see LICENSE for details. */
 /* SPDX-License-Identifier: Apache-2.0 */
 /*

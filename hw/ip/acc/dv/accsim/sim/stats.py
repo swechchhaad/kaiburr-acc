cc96443ca889e727a6f8f761d3b38b2a5b0fc657
Swechchha Adhikari <swechchha.adhikari4@gmail.com>
2026-07-02 17:06:25 +0200

    test: skip all non-function symbols

--- hw/ip/acc/dv/accsim/sim/stats.py
+++ hw/ip/acc/dv/accsim/sim/stats.py
@@ -210,8 +210,9 @@ def _get_addr_symbol_map(elf_file: ELFFile) -> Dict[int, str]:
 
     return {sym.entry.st_value: sym.name
             for sym in section.iter_symbols()
-            # test: skip ELF mapping symbols
-            if sym.entry['st_shndx'] == 1 and not sym.name.startswith('$')}
+            # skip non-function symbols living in .text: $d/$x
+            if sym.entry['st_shndx'] == 1
+            and not sym.name.startswith(('$', '.'))}
 
 
 class ExecutionStatAnalyzer:
